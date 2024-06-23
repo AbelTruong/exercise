9b22ec16d347208fc63f542d5cf5f54e51c5e059
abeltruong <abel.t@beginningboutique.com.au>
2024-06-23 15:03:15 +0700

    add countdown timer 23/06/24

---
+++ countdown_timer/main.js
@@ -0,0 +1,67 @@
+const countdownElement = document.getElementById('countdown')
+
+function CountDownTimer() {
+  const now = new Date()
+  const targetTime = new Date()
+  targetTime.setHours(15, 1, 0, 0)
+
+  if (now > targetTime) {
+    window.localStorage.removeItem('countdownTimer')
+    return {
+      showCountDownTimer: false,
+      timer: {
+        h: 0,
+        m: 0,
+        s: 0,
+      },
+    }
+  } else {
+    const diff = targetTime - now
+    const hours = Math.floor(diff / (1000 * 60 * 60))
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000)
+
+    window.localStorage.setItem('countdownTimer', diff)
+    return {
+      showCountDownTimer: true,
+      timer: {
+        h: hours,
+        m: minutes,
+        s: seconds,
+      },
+    }
+  }
+}
+
+function renderPlural(number) {
+  return number > 1 ? 'S ' : ' '
+}
+
+function handleCountdown() {
+  function updateCountdown() {
+    let { showCountDownTimer, timer } = CountDownTimer()
+
+    countdownElement.innerHTML = ''
+
+    if (showCountDownTimer) {
+      countdownElement.innerHTML += `<h2 id="countdown_timer">
+        WELCOME, ${timer.h} HOUR${renderPlural(timer.h)} ${timer.m} 
+        MIN${renderPlural(timer.m)} ${timer.s} SECOND${renderPlural(timer.s)} IS THE END OF WORKING
+        TIME
+      </h2>`
+    } else {
+      countdownElement.innerHTML += `<h2 id="countdown_timer">
+        Abel is back
+      </h2>`
+      clearInterval(countDown)
+    }
+  }
+
+  updateCountdown()
+  const countDown = setInterval(updateCountdown, 1000)
+}
+
+window.addEventListener('load', handleCountdown)
+window.addEventListener('beforeunload', function () {
+  window.localStorage.removeItem('countdownTimer')
+})
