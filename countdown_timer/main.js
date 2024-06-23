f4625d93fb958b6b3d4b94bb37c8c7b92e534574
abeltruong <abel.t@beginningboutique.com.au>
2024-06-23 15:04:52 +0700

    update countdown timer 23/06/24

--- countdown_timer/main.js
+++ countdown_timer/main.js
@@ -1,12 +1,12 @@
-const countdownElement = document.getElementById('countdown')
+const countdownElement = document.getElementById("countdown");
 
 function CountDownTimer() {
-  const now = new Date()
-  const targetTime = new Date()
-  targetTime.setHours(15, 1, 0, 0)
+  const now = new Date();
+  const targetTime = new Date();
+  targetTime.setHours(17, 0, 0, 0);
 
   if (now > targetTime) {
-    window.localStorage.removeItem('countdownTimer')
+    window.localStorage.removeItem("countdownTimer");
     return {
       showCountDownTimer: false,
       timer: {
@@ -14,14 +14,14 @@ function CountDownTimer() {
         m: 0,
         s: 0,
       },
-    }
+    };
   } else {
-    const diff = targetTime - now
-    const hours = Math.floor(diff / (1000 * 60 * 60))
-    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
-    const seconds = Math.floor((diff % (1000 * 60)) / 1000)
+    const diff = targetTime - now;
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-    window.localStorage.setItem('countdownTimer', diff)
+    window.localStorage.setItem("countdownTimer", diff);
     return {
       showCountDownTimer: true,
       timer: {
@@ -29,39 +29,39 @@ function CountDownTimer() {
         m: minutes,
         s: seconds,
       },
-    }
+    };
   }
 }
 
 function renderPlural(number) {
-  return number > 1 ? 'S ' : ' '
+  return number > 1 ? "S " : " ";
 }
 
 function handleCountdown() {
   function updateCountdown() {
-    let { showCountDownTimer, timer } = CountDownTimer()
+    let { showCountDownTimer, timer } = CountDownTimer();
 
-    countdownElement.innerHTML = ''
+    countdownElement.innerHTML = "";
 
     if (showCountDownTimer) {
       countdownElement.innerHTML += `<h2 id="countdown_timer">
         WELCOME, ${timer.h} HOUR${renderPlural(timer.h)} ${timer.m} 
         MIN${renderPlural(timer.m)} ${timer.s} SECOND${renderPlural(timer.s)} IS THE END OF WORKING
         TIME
-      </h2>`
+      </h2>`;
     } else {
       countdownElement.innerHTML += `<h2 id="countdown_timer">
         Abel is back
-      </h2>`
-      clearInterval(countDown)
+      </h2>`;
+      clearInterval(countDown);
     }
   }
 
-  updateCountdown()
-  const countDown = setInterval(updateCountdown, 1000)
+  updateCountdown();
+  const countDown = setInterval(updateCountdown, 1000);
 }
 
-window.addEventListener('load', handleCountdown)
-window.addEventListener('beforeunload', function () {
-  window.localStorage.removeItem('countdownTimer')
-})
+window.addEventListener("load", handleCountdown);
+window.addEventListener("beforeunload", function () {
+  window.localStorage.removeItem("countdownTimer");
+});
